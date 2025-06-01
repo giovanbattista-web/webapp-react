9167b41f723f2e4faac9ed4f6e66c6bd08c5a298
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-06-01 09:48:56 +0200

    [UPD] COMPONENTS CON FILE HEADER.JSX ED AGGIUNTA DI LINK

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav className='navbar bg-primary mb-4'>
+            <div className='container-fluid'>
+                <Link className='navbar-brand' to="/">Movies</Link>
+            </div>
+
+        </nav>
+    )
+};
+
+export default Header;
