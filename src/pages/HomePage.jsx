0d803b32d3e9e3bddf1460f20dacc02ebb07cea5
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-06-02 07:15:39 +0200

    [UPD] HOMEPAGE

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,45 @@ const HomePage = () => {
         <>
             <h1 className='text-primary'>Movies</h1>
             <h2><i>The movie community</i></h2>
-            <div className='row'></div>
+            <div className='row gy-4'>
+                <div className='col-12 col-md-6 col-lg-4 '>
+                    <div className='card'>
+                        <div className='card-image'>
+                            <img src="https://picsum.photos/400/300" className='img-fluid' alt="" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-12 col-md-6 col-lg-4 '>
+                    <div className='card'>
+                        <div className='card-image'>
+                            <img src="https://picsum.photos/400/300" className='img-fluid' alt="" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-12 col-md-6 col-lg-4 '>
+                    <div className='card'>
+                        <div className='card-image'>
+                            <img src="https://picsum.photos/400/300" className='img-fluid' alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className='col-12 col-md-6 col-lg-4 '>
+                    <div className='card'>
+                        <div className='card-image'>
+                            <img src="https://picsum.photos/400/300" className='img-fluid' alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className='col-12 col-md-6 col-lg-4 '>
+                    <div className='card'>
+                        <div className='card-image'>
+                            <img src="https://picsum.photos/400/300" className='img-fluid' alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 };
